ee7299140ceb3865c0db35fe5f32c092a9a73bc5
Wlad K <playerw00@googlemail.com>
2017-11-30 20:43:47 +0100

    update domtree

--- src/js/elementManipulation/pageDomTree.js
+++ src/js/elementManipulation/pageDomTree.js
@@ -1,108 +1,126 @@
-/* global $ */
-/* show full page dom
-  building the tree almost takes no time (after the change to append the whole tree in one append command),
-  because of that it gets rebuild after every action
+/* global Event */
+/** show full page dom
+  * building the tree almost takes no time,
+  * because of that it gets rebuild after every action
 */
 const pageDomTree = {
-  idCounter: 0,
-  SimulatetAndTreeReleation: [], // releation between the dom tree and the real one
-  tree: [], // done tree
-  openedTree: [],
+  /**
+  * @example {
+  *   html: '',
+  *   ids: [],
+  *   counter: 0
+  *}
+  * @param {String} treeData.html - html to append
+  * @param {HTMLElement[]} treeData.ids - all iframe dom Elements
+  * @param {Number} treeData.counter - id counter
+  */
+  treeData: null,
   hasEvents: false,
-  iconShow: 'fa fa-caret-down',
-  iconHide: 'fa fa-caret-right',
-  iconNoChildren: 'fa fa-angle-right',
 
   // build whole tree with reset
   build (iframe, domTree) {
-    this.$simulated = iframe || $('#simulated').contents().find('body')
-    this.$simulatedDomTree = domTree || $('#simulatedDomTree')
-    let tree = []
-    let html = {val: ''}
+    this.$iframe = iframe || document.getElementById('simulated').contentDocument.body
+    this.$domTree = domTree || document.getElementById('simulatedDomTree')
+    this.treeData = {html: '', ids: [], counter: 0}
+
+    if (!this.$iframe && !this.$domTree) {
+      return false
+    }
 
     this.reset()
-      .createTree(this.$simulated, tree, html)
-      .$simulatedDomTree.html(html.val)
-
-    this.tree = tree
+      .createTree(this.$iframe)
+      .$domTree.innerHTML = this.treeData.html
 
     if (!this.hasEvents) { // add events if not exist
       this.addOpenCloseEvents()
-        .addEvents()
+        .addRelationEvents(this.treeData.ids)
       this.hasEvents = true
     }
   },
 
   // reset to default state
   reset () {
-    this.idCounter = 0
-    this.$simulatedDomTree.html('')
-    this.tree = []
+    this.$domTree.innerHTML = ''
     return this
   },
 
-  isTreeNodeParent (node) {
-    return ($(node).children().length > 0)
-  },
-
-  // create a dom tree with a recursive function
-  // the completed tree will be saved in this.tree[]
-  // tests made 30 times and getting min to max
-  // append html after done = 6ms ~ 12ms
-  // append each element itself = 260ms ~ 340ms
-  createTree (node, tree, html) {
-    let children = node.children()
+  /** Create a dom tree with a recursive function.
+  * Append html after done = ~ 5ms.
+  * Append each element itself = ~ 300ms.
+  * this is using the whole this.treeData object, please check it out before
+  * reading the following.
+  */
+  createTree (node) {
+    let children = node.children
     for (var i = 0; i < children.length; i++) {
-      if (this.isTreeNodeParent(children[i])) {
-        tree.push(children[i])
-        html.val += `<ul class="hidden" class="tree-item" id="tree-${this.idCounter++}">
-          <i class="${this.iconHide}" aria-hidden="true"></i>
-          <p>${children[i].nodeName}  <span class="classes">${$(children[i]).attr('class') || ''}<span></p>`
-        this.createTree($(children[i]), tree, html)
-        html.val += `</ul>`
-        continue
-      }
-
-      tree.push(children[i])
-      html.val += `<li id="tree-${this.idCounter++}" class="tree-item">
-        <i class="${this.iconNoChildren}" aria-hidden="true"></i>
-        ${children[i].nodeName}  <span class="classes">${$(children[i]).attr('class') || ''}<span>
+      if (children[i].children.length > 0) {
+        this.treeData.ids.push(children[i])
+        children[i].domTree = this.treeData.counter
+        this.treeData.html += `<ul class="hidden" class="tree-item" id="tree-${this.treeData.counter++}">
+          <i class="fa fa-caret-right" aria-hidden="true"></i>
+          <p>${children[i].nodeName}
+           <span class="classes">${children[i].getAttribute('class') || ''}</span>
+          </p>`
+        this.createTree(children[i])
+        this.treeData.html += `</ul>`
+      } else {
+        this.treeData.ids.push(children[i])
+        children[i].domTree = this.treeData.counter
+        this.treeData.html += `<li class="tree-item" id="tree-${this.treeData.counter++}">
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+        ${children[i].nodeName}
+        <span class="classes">${children[i].getAttribute('class') || ''}</span>
         </li>`
+      }
     }
     return this
   },
 
   // add click and mouseover events to appended dom items
-  addEvents () {
-    this.$simulatedDomTree.off('click.domtreeSimulate').on('click.domtreeSimulate', 'ul, li', (e) => {
-      e.stopImmediatePropagation()
-      let treeNode = $(e.currentTarget).attr('id').split('-')[1]
-      $(this.tree[treeNode]).trigger('click')
-    })
-
-    this.$simulatedDomTree.off('mouseover.domtreeSimulate').on('mouseover.domtreeSimulate', 'ul, li', (e) => {
-      e.stopImmediatePropagation()
-      let treeNode = $(e.currentTarget).attr('id').split('-')[1]
-      $(this.tree[treeNode]).trigger('mouseover')
-    })
+  addRelationEvents (tree) {
+    window.domTree = tree
+    this.$domTree.removeEventListener('mousedown', this.addRelationClickEvents)
+    this.$domTree.addEventListener('mousedown', this.addRelationClickEvents)
+    this.$domTree.removeEventListener('mouseover', this.addRelationHoverEvents)
+    this.$domTree.addEventListener('mouseover', this.addRelationHoverEvents)
     return this
+  },
+
+  addRelationHoverEvents (e) {
+    let el = e.target.closest('li, ul')
+    if (el) {
+      let event = new Event('mouseover')
+      window.domTree[el.getAttribute('id').split('-')[1]].dispatchEvent(event)
+    }
+  },
+
+  addRelationClickEvents (e) {
+    let el = e.target.closest('li, ul')
+    if (el) {
+      let event = new Event('mousedown')
+      window.domTree[el.getAttribute('id').split('-')[1]].dispatchEvent(event)
+    }
   },
 
   // add caret icons for better navigation
   // all ul items can be expanded with a click
   addOpenCloseEvents () {
-    this.$simulatedDomTree.off('click.domtree').on('click.domtree', 'i', (e) => {
-      let ul = $(e.target).parent()
-      let item = $(e.target)
-      if (ul.hasClass('hidden')) { // if not visible, show it
-        item.attr('class', this.iconShow)
-        ul.removeClass('hidden')
-      } else { // if visivle, hide it
-        item.attr('class', this.iconHide)
-        ul.addClass('hidden')
-      }
-    })
+    this.$domTree.removeEventListener('mousedown', this.addOpenCloseEvent)
+    this.$domTree.addEventListener('mousedown', this.addOpenCloseEvent)
     return this
+  },
+
+  addOpenCloseEvent (e) {
+    if (e.target.tagName === 'I') {
+      let ul = e.target.parentNode
+      if (ul.classList.contains('hidden')) {
+        e.target.setAttribute('class', 'fa fa-caret-down')
+        ul.classList.remove('hidden')
+      } else {
+        e.target.setAttribute('class', 'fa fa-caret-right')
+        ul.classList.add('hidden')
+      }
+    }
   }
 }
 module.exports = pageDomTree
