7a5f280bb1b00ab59063e8bbf1c5ff6f61855710
Wlad K <playerw00@googlemail.com>
2017-12-14 20:03:34 +0100

    finally removing Jquery from the end product

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const $ = require('jquery')
-window.$ = $
 require('./style/main.scss')
 
 const elementEvents = require('./js/elementManipulation/elementEvents.js') // all element events
@@ -7,13 +5,14 @@ const elementItems = require('./js/newElements/elementItems.js') // all element 
 const navigation = require('./js/navigation.js') // standalone navigation
 const menu = require('./js/menu/buildMenu')
 
-$(document).ready(function ($) {
+document.addEventListener('DOMContentLoaded', () => {
   elementEvents.init()
   menu.build(elementEvents)
   elementItems.init(elementEvents)
   navigation.init()
 
-  $('iframe').ready(() => {
+  // iframe ready
+  document.getElementById('simulated').contentDocument.addEventListener('DOMContentLoaded', (event) => {
     elementEvents.initAfterFrame()
   })
 })
