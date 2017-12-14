f9893b3aec3bdba1bc9a4e00e375c21cad8eb9a1
Wlad K <playerw00@googlemail.com>
2017-12-14 20:02:52 +0100

    added categories to the newElements and rewrote the design and js for it

--- src/js/newElements/basicItems.js
+++ src/js/newElements/basicItems.js
@@ -1,113 +1,120 @@
-const basicElementItems = [
-  {
-    name: 'Layout',
-    items: [
-      {
-        name: 'Section',
-        icon: require('../../img/test.svg'),
-        tags: ['section', 'sections'],
-        info: '',
-        html: `<section></section>`
-      },
-      {
-        name: 'Container',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<div></div>`
-      },
-      {
-        name: 'Colum',
-        icon: require('../../img/test.svg'),
-        run: ''
-      }
-    ]
-  },
-  {
-    name: 'Typography',
-    items: [
-      {
-        name: 'Heading',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<h1> Lorem ipsum dolor sit amet </h1>`
-      },
-      {
-        name: 'Paragraph',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<p> Lorem ipsum dolor sit amet </p>`
-      },
-      {
-        name: 'Link',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<a href="#"> Lorem ipsum dolor sit amet </a>`
-      },
-      {
-        name: 'Blockquote',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<blockquote> Lorem ipsum dolor sit amet </blockquote>`
-      }
-    ]
-  },
-  {
-    name: 'Media',
-    items: [
-      {
-        name: 'Image',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<img src="#" alt="" >`
-      },
-      {
-        name: 'Icon',
-        icon: require('../../img/test.svg'),
-        info: '',
-        run: ''
-      },
-      {
-        name: 'Video',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<video width="400" controls>
-                  <source src="" type="">
-                  Your browser does not support HTML5 video.
-               </video>`
-      }
-    ]
-  },
-  {
-    name: 'Forms',
-    items: [
-      {
-        name: 'From Block',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<form class="" action="" method="post">
-              </form>`
-      },
-      {
-        name: 'Field Label',
-        icon: require('../../img/test.svg'),
-        info: '',
-        html: `<form class="" action="" method="post">
-              </form>`
-      }
-    ]
-  },
-  {
-    name: 'Page',
-    items: [
+const basicElementItems = {
+  id: 'gwvpBasicItems',
+  name: 'Basic',
+  info: 'Basic HTML elements without any styling',
+  icon: require('../../img/test.svg'),
+  css: '',
+  content: [
+    {
+      name: 'Layout',
+      items: [
+        {
+          name: 'Section',
+          icon: require('../../img/test.svg'),
+          tags: ['section', 'sections'],
+          info: '',
+          html: `<section></section>`
+        },
+        {
+          name: 'Container',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<div></div>`
+        },
+        {
+          name: 'Colum',
+          icon: require('../../img/test.svg'),
+          run: ''
+        }
+      ]
+    },
+    {
+      name: 'Typography',
+      items: [
+        {
+          name: 'Heading',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<h1> Lorem ipsum dolor sit amet </h1>`
+        },
+        {
+          name: 'Paragraph',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<p> Lorem ipsum dolor sit amet </p>`
+        },
+        {
+          name: 'Link',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<a href="#"> Lorem ipsum dolor sit amet </a>`
+        },
+        {
+          name: 'Blockquote',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<blockquote> Lorem ipsum dolor sit amet </blockquote>`
+        }
+      ]
+    },
+    {
+      name: 'Media',
+      items: [
+        {
+          name: 'Image',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<img src="#" alt="" >`
+        },
+        {
+          name: 'Icon',
+          icon: require('../../img/test.svg'),
+          info: '',
+          run: ''
+        },
+        {
+          name: 'Video',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<video width="400" controls>
+                    <source src="" type="">
+                    Your browser does not support HTML5 video.
+                 </video>`
+        }
+      ]
+    },
+    {
+      name: 'Forms',
+      items: [
+        {
+          name: 'From Block',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<form class="" action="" method="post">
+                </form>`
+        },
+        {
+          name: 'Field Label',
+          icon: require('../../img/test.svg'),
+          info: '',
+          html: `<form class="" action="" method="post">
+                </form>`
+        }
+      ]
+    },
+    {
+      name: 'Page',
+      items: [
 
-    ]
-  },
-  {
-    name: 'Misc',
-    items: [
+      ]
+    },
+    {
+      name: 'Misc',
+      items: [
 
-    ]
-  }
-]
+      ]
+    }
+  ]
+}
 
 module.exports = basicElementItems
--- src/js/newElements/elementItems.js
+++ src/js/newElements/elementItems.js
@@ -1,50 +1,116 @@
-/* global $ */
 const basicElementItems = require('./basicItems')
 const elementItems = {
+  /** requires elementEvents to drag elements */
   elementEvents: null,
-  iconShow: 'fa fa-caret-down',
-  iconHide: 'fa fa-caret-right',
+  currentOpenTarget: null,
+
   init (elementEvents) {
     this.elementEvents = elementEvents
-    this.cacheDom()
-    this.buildHtml()
-    this.bindEvents()
+    elementItemsHTML.addUsingCategory(basicElementItems)
+    this.bindUsingEvents()
   },
-  cacheDom () {
-    this.$basicDomElement = $('.sidebar-basic')
-  },
-  bindEvents () {
-    $('.sidebar-visible-header').on('click', (e) => {
-      let sibling = $(e.currentTarget).siblings()
-      this.toggleClass(sibling, 'hidden')
+
+  bindUsingEvents () {
+    let all = document.getElementById('sidebar-basic')
+    all.addEventListener('mousedown', (e) => {
+      let item = e.target.closest('.html-element')
+      if (item) {
+        this.dragStart(item)
+      }
+
+      let itemCategory = e.target.closest('.sidebar-visible-header')
+      if (itemCategory) {
+        this.toggleClass(itemCategory)
+        return
+      }
+
+      let contentContainer = e.target.closest('.newElements-content')
+      if (contentContainer) {
+        return
+      }
+
+      let bigCategory = e.target.closest('.newElements-item')
+      if (bigCategory) {
+        this.showBigCategory(bigCategory)
+      }
     })
-    $('.html-element').on('mousedown', (e) => {
-      let html = unescape($(e.currentTarget).data('html'))
-      this.elementEvents.dragStart(html)
-    })
   },
-  toggleClass (item, Class) {
-    if (item.hasClass('hidden')) {
-      item.removeClass('hidden')
+
+  showBigCategory (category) {
+    this.hideBigCategories()
+    if (this.currentOpenTarget === category) {
+      this.currentOpenTarget = null
+      return
+    }
+    let content = document.getElementById('content-' +
+      category.getAttribute('id'))
+    category.classList.add('active')
+    content.classList.remove('hidden')
+
+    this.currentOpenTarget = category
+  },
+
+  hideBigCategories () {
+    Array.from(document.getElementsByClassName('newElements-item'))
+      .forEach((element) => {
+        if (element.classList.contains('active')) {
+          element.classList.remove('active')
+        }
+      })
+    Array.from(document.getElementsByClassName('newElements-content'))
+      .forEach((element) => {
+        if (!element.classList.contains('hidden')) {
+          element.classList.add('hidden')
+        }
+      })
+  },
+
+  toggleClass (target) {
+    let item = target.nextElementSibling
+    if (item.classList.contains('hidden')) {
+      item.classList.remove('hidden')
     } else {
-      item.addClass('hidden')
+      item.classList.add('hidden')
     }
   },
-  buildHtml () {
-    let html = ''
-    for (var i = 0; i < basicElementItems.length; i++) {
-      // header
+
+  dragStart (target) {
+    let html = unescape(target.getAttribute('html'))
+    this.elementEvents.dragStart(html)
+  }
+}
+module.exports = elementItems
+
+const elementItemsHTML = {
+  $using: document.getElementById('newElementsUsing'),
+  $official: document.getElementById('newElementsOfficial'),
+  $online: document.getElementById('newElementsOnline'),
+
+  templateBigCategory (id, name, img) {
+    return `
+    <div class="newElements-item" id=${id}>
+      <div class="img">
+        <img src="${img}" alt="image of ${name}">
+      </div>
+      <p>${name}</p>
+    </div>
+    `
+  },
+
+  templateItemCategories (id, data) {
+    let html = `<div class="newElements-content hidden" id="content-${id}">`
+    for (var i = 0; i < data.length; i++) {
       html += `
-      <div class="sidebar-header sidebar-elements">
+      <div class="sidebar-header sidebar-element">
         <div class="sidebar-visible-header">
-          <i class="${this.iconShow}" aria-hidden="true"></i>
-          <p>${basicElementItems[i].name}</p>
+          <i class="fa fa-caret-down" aria-hidden="true"></i>
+          <p>${data[i].name}</p>
         </div>
         <div class="items hidden">`
-      let items = basicElementItems[i].items
+      let items = data[i].items
       for (var k = 0; k < items.length; k++) {
         html += `
-        <div class="item html-element" data-html=${escape(items[k].html)}>
+        <div class="item html-element" html=${escape(items[k].html)}>
           <p class="helpIcon">?</p>
           <div class="img">
             <img src="${items[k].icon}" alt="${items[k].name}">
@@ -56,10 +122,26 @@ const elementItems = {
           </div>
         </div>`
       }
-      html += `</div>
-      </div>`
+      html += ` </div>
+        </div>`
     }
-    this.$basicDomElement.append(html)
+    return html
+  },
+
+  addUsingCategory (data, existing) {
+    if (existing) {
+      let old = existing.outerhtml
+      this.$using.innerHTML += old
+      old = ''
+    } else {
+      this.$using.innerHTML +=
+        this.templateBigCategory(data.id, data.name, data.icon)
+    }
+    document.getElementById(data.id).innerHTML +=
+      this.templateItemCategories(data.id, data.content)
+  },
+
+  addOnlineCategory (data) {
+
   }
 }
-module.exports = elementItems
