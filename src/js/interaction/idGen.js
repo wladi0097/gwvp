81264ae616d5e126b89087858d3218364c4d4da2
Wlad K <playerw00@googlemail.com>
2017-12-21 22:19:06 +0100

    rename method and add source to idGen

--- src/js/interaction/idGen.js
+++ src/js/interaction/idGen.js
@@ -1,11 +1,21 @@
+/** @see https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+ * a simple id Generator
+ */
 const idGen = {
-  get () {
+  /**
+   * get a new unique id
+   * @return {String} id
+   */
+  new () {
     return this.idGenerator() + this.idGenerator() +
     '-' + this.idGenerator() + '-' + this.idGenerator() + '-' +
     this.idGenerator() + '-' + this.idGenerator() + this.idGenerator() +
     this.idGenerator()
   },
 
+  /** creates a random number sequence
+    @return {String} number sequence
+   */
   idGenerator () {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
